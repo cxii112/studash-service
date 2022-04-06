175dc77f9db22673a769e1944ca87000aa1e8183
cxii112 <lyakhovoy.d@ya.ru>
2022-04-06 09:05:08 +0500

    Контроллер API расписания

---
+++ studash-service/Controllers/ScheduleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using studash_service.Context;
+
+namespace studash_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScheduleController : ControllerBase
+    {
+        private Dictionary<string, UniversityContext> _contexts;
+
+        public ScheduleController()
+        {
+            _contexts =
+                new Dictionary<string, UniversityContext>
+                {
+                    { "Хогвардс", new UniversityContext("Hogwards") }
+                };
+        }
+
+        [HttpPost]
+        public IEnumerable<Lesson> GetAll(FullScheduleRequest request)
+        {
+            var context = _contexts[request.University];
+            return context.schedule
+                          .Where(lesson => lesson.group_name == request.GroupName)
+                          .ToArray();
+        }
+    }
+}
