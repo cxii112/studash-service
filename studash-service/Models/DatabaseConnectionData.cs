f267a7d037ca0482aac04f690e9aaf8601f714c1
cxii112 <lyakhovoy.d@ya.ru>
2022-04-06 08:57:23 +0500

    Интерфейс данных о БД
    Сами данные не загружаются в репозиторий, из-за наличия паролей

---
+++ studash-service/Models/DatabaseConnectionData.cs
@@ -0,0 +1,19 @@
+﻿namespace studash_service.Context
+{
+    public class DatabaseConnectionData : IDatabaseConnectionData
+    {
+        public string Host { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    
+    
+    public interface IDatabaseConnectionData
+    {
+        public string Host { get; set; }
+        public string Database { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
