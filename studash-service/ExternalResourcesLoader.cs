63681824272587cbda91aad142ee42910890dca2
cxii112 <lyakhovoy.d@ya.ru>
2022-04-06 08:58:45 +0500

    Получение данных о БД из внешнего файла
    Пока захардкожено

---
+++ studash-service/ExternalResourcesLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using studash_service.Context;
+
+namespace studash_service
+{
+    public class ExternalResourcesLoader
+    {
+        public static Dictionary<string,DatabaseConnectionData> LoadDatabasesData()
+        {
+            string jsonString = File
+                .ReadAllText("C:/Repository/C#/studash-service/studash-service/databases.json");
+            return JsonSerializer.Deserialize<Dictionary<string,DatabaseConnectionData>>(jsonString);
+        }
+
+        public static IDatabaseConnectionData LoadDatabaseData(string connectionName)
+        {
+            var databasesData = LoadDatabasesData();
+            return databasesData[connectionName];
+        }
+    }
+}
