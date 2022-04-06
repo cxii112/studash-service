019fee30bbd3a9953eed6673650adcb968caae52
cxii112 <lyakhovoy.d@ya.ru>
2022-04-06 09:00:57 +0500

    data-class данных об занятии
    отражает поля базы данных

---
+++ studash-service/Models/Lesson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace studash_service.Context
+{
+    public class Lesson
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+
+        public DateTime date { get; set; }
+        public string group_name { get; set; }
+        public bool has_lessons { get; set; }
+        public TimeSpan start_time { get; set; }
+        public TimeSpan end_time { get; set; }
+        public string subject { get; set; }
+        public string teacher { get; set; }
+        public string auditorium { get; set; }
+        public string building { get; set; }
+    }
+}
