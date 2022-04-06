5864206eb3a6eb597c44e99be630c26b9cf29baa
cxii112 <lyakhovoy.d@ya.ru>
2022-04-06 09:04:47 +0500

    Формат запроса данных

---
+++ studash-service/Models/FullScheduleRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace studash_service.Context
+{
+    public class FullScheduleRequest
+    {
+        public string GroupName { get; set; }
+        public string University { get; set; }
+    }
+}
