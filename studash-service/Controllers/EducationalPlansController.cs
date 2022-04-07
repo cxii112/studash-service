7ff2419c716e93cd0d14286ebbd826b3fb22d270
cxii112 <lyakhovoy.d@ya.ru>
2022-04-07 18:33:29 +0500

    Новый контроллер учебных планов

--- studash-service/Controllers/EducationalPlansController.cs
+++ studash-service/Controllers/EducationalPlansController.cs
@@ -7,11 +7,12 @@ using AspNetCore.Yandex.ObjectStorage;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using studash_service.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace studash_service.Controllers
 {
     [ApiController]
-    [Route("plan")]
+    [Route("[controller]")]
     public class EducationalPlansController : ControllerBase
     {
         private readonly Dictionary<string, EducationalPlanContext> _contexts;
@@ -31,15 +32,47 @@ namespace studash_service.Controllers
         }
 
         [HttpPost]
-        [Route("{specialityCode}")]
-        public JsonDocument GetPlan(PlanRequest request, string specialityCode)
+        [Route("plan")]
+        [SwaggerOperation(Summary = "Запрос на получение учебного плана", Description = "Запрашивает из базы данных путь к файлу на удаленном хранилище")]
+        [SwaggerResponse(201, "Возвращает файл учебного плана")]
+        [SwaggerResponse(204, "Данные не найдены или не правильно составлен запрос")]
+        [SwaggerResponse(500, "Произошла ошибка при получении расписания")]
+        public ObjectResult GetPlan(PlanRequest request)
         {
-            _logger.Log(LogLevel.Information,$"[{Request.Method}] {Request.Host.Host}{Request.Path.Value} @ {DateTime.UtcNow}");
-            var context = _contexts[request.Univercity];
-            var plan = context.educationalplans
-                              .Single(plan => plan.specialitycode == specialityCode);
-            Stream stream = _storage.GetAsStreamAsync(plan.planreference).Result;
-            return JsonDocument.Parse(stream);
+            _logger.LogInformation($"[{Request.Method}] {Request.Host.Host}{Request.Path.Value} {DateTime.UtcNow}");
+            try
+            {
+                var context = _contexts[request.University];
+                var plan = context.educationalplans
+                                  .Single(plan => plan.specialitycode == request.SpecialityCode);
+                
+                Stream stream = _storage.GetAsStreamAsync(plan.planreference).Result;
+                return Ok(JsonDocument.Parse(stream));
+            }
+            catch (KeyNotFoundException)
+            {
+                int statusCode = 500;
+                _logger.LogError($"{statusCode} {DateTime.UtcNow} ({Request.Body})");
+                return Problem(statusCode: statusCode, detail: $"Университет не найден");
+            }
+            catch (InvalidOperationException)
+            {
+                int statusCode = 500;
+                _logger.LogError($"{statusCode} {DateTime.UtcNow} ({Request.Body})");
+                return Problem(statusCode: statusCode, detail: $"Неверный код специальности");
+            }
+            catch (ArgumentNullException)
+            {
+                int statusCode = 500;
+                _logger.LogError($"{statusCode} {DateTime.UtcNow} ({Request.Body})");
+                return Problem(statusCode: 500, detail: $"Необходимо указать Университет");
+            }
+            catch (Exception e)
+            {
+                int statusCode = 500;
+                _logger.LogError($"{statusCode} {DateTime.UtcNow} ({Request.Body}) {e.Message}");
+                return Problem(statusCode: 500, detail: $"Internal error");
+            }
         }
     }
 }
