3d6aef97b8dbe9d56711a8ed916118dab02c8514
cxii112 <lyakhovoy.d@ya.ru>
2022-04-07 16:10:28 +0500

    реефакторинг

---
+++ studash-service/Models/ScheduleContext.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCore.Yandex.ObjectStorage;
+using Microsoft.EntityFrameworkCore;
+
+namespace studash_service.Models
+{
+    public class ScheduleContext : UniversityContext
+    {
+        public ScheduleContext(string key, YandexStorageService storage) : base(key, storage) { }
+        public DbSet<Lesson> schedule { get; set; }
+    }
+}
