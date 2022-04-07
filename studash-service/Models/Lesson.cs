4373864b9ad8319c5b6df3b0573ed34bdf957ebf
cxii112 <lyakhovoy.d@ya.ru>
2022-04-07 21:16:39 +0500

    Изменение формата данных

--- studash-service/Models/Lesson.cs
+++ studash-service/Models/Lesson.cs
@@ -11,8 +11,8 @@ namespace studash_service.Models
         public DateTime date { get; set; }
         public string group_name { get; set; }
         public bool has_lessons { get; set; }
-        public TimeSpan start_time { get; set; }
-        public TimeSpan end_time { get; set; }
+        public DateTime start_time { get; set; }
+        public DateTime end_time { get; set; }
         public string subject { get; set; }
         public string teacher { get; set; }
         public string auditorium { get; set; }
